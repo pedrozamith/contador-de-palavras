ca94c426fae65c708d5361e72dae1bddda0fa9a6
pedrozamith <pedrozamith@hotmail.com.br>
2017-08-14 16:58:30 -0300

    Analisei cada caso e estabeleci um contador eficiente, que peca somente em um tipo de problema (float com pontuação em seguida)

--- src/main.c
+++ src/main.c
@@ -1,22 +1,69 @@
-/* Contador de palavras
- *
- * Este programa recebera uma serie de caracteres representando palavras em sua
- * entrada. Ao receber um caractere fim de linha ('\n'), deve imprimir na tela o
- * numero de palavras separadas que recebeu e, apos, encerrar.
- */
-
 #include <stdio.h>
 
-int main() {
+int main (){
 
-  char c;
+	int  n_palavra = 0 , dentro_palavra = 0, flag_num = 0, flag_ponto = 0, esteve_float = 0;
+	char frase;
 
-  c = 1;
+	while (frase != '\n'){
+	  scanf ("%c", &frase);
+	  frase = frase + NULL;
+	  
+		if (frase > 64) {   				//verifica se o caractere é especial ou letra. 
+			dentro_palavra = 1;			//caso seja letra, flag ativada
+			flag_num = 0;
+			flag_ponto = 0;
+		}
+		
+		else if (flag_num == 1){			//trata o caso de ponto flutuante 
+		if (!((frase > 47)&&(frase < 58))){
+			if ((frase == 44)||(frase == 46)){	//Se for . ou , ativa flag de ponto flutuante
+				flag_ponto++;
+				esteve_float = 1;
+			}
+		
+		n_palavra++;
+		dentro_palavra = 0;
+		flag_num = 0;
+		flag_ponto = 0;	
+	
+		}
+		
+		else if (flag_ponto > 1){		//trata o caso de pontuação em seguida do ponto flutuante
+					n_palavra++;
+					dentro_palavra = 0;
+					flag_num = 0;
+					flag_ponto = 0;	
+					esteve_float = 1;
+		}
+		
+		else {
+			flag_num = 1;
+			dentro_palavra = 1;
+		}
+							
+			}
+			
 
-  while (c != '\n') {
-    scanf("%c", &c);
-  }
-
-  printf("1\n");
-  return 0;
+		else if ((frase > 47)&&(frase < 58)){		//trata o caso de numeros
+			flag_num = 1;				//caso apareça algum numero, a verificação ocorre até aparecer um
+			dentro_palavra = 1;			//caractere especial. Se for . ou , não conta mais mas se for espaço
+		}								//ele conta como mais uma palavra
+	  
+	  	else { 
+			if (dentro_palavra == 1){
+		 	 	n_palavra++;
+		 		dentro_palavra = 0;
+				flag_num = 0;
+				flag_ponto = 0;
+			}
+	  	}
+	}
+	
+	if (esteve_float == 1){
+		n_palavra--;
+		
+	}
+printf ("%d",n_palavra);
+	return 0;
 }
